ccee3fe3642e86fc16b8bf9871bbdb55ad197e50
暁东邪 <xiaodongxiexie@users.noreply.github.com>
2021-08-16 09:33:54 +0800

    Update api.py

--- ty/script/api.py
+++ ty/script/api.py
@@ -14,41 +14,9 @@ import aiohttp
 
 from script import table
 from script.table import session, _bulk_insert
-
+from script.config import URL, URLS_DICT, HEADERS, DATA
 
 logger = logging.getLogger("aiops")
-
-
-# -------------------------------------------------------------------------------------------------
-URL = "https://api.tingyun.com/server/latest/accounts/1635961/charts/account-applications.json"
-
-URL_PREFIX = "https://api.tingyun.com/server/latest/accounts/1635961/application/{applicationId}/charts"
-
-URL_APDEX = "%s/application-apdex.json" % URL_PREFIX
-URL_ERRORS = "%s/application-errors.json" % URL_PREFIX
-URL_TOPN = "%s/application-webaction-topn.json" % URL_PREFIX
-URL_APPLICATION = "%s/application-application.json" % URL_PREFIX
-
-URL_THROUGHPUT = "%s/application-throughput.json" % URL_PREFIX
-URL_OVERVIEW_QUANTILE = "%s/application-overview-quantile.json" % URL_PREFIX
-URL_CPU = "%s/application-cpu.json" % URL_PREFIX
-URL_MEM = "%s/application-mem.json" % URL_PREFIX
-
-HEADERS = {"X-Auth-Key": "dnngfb73"}
-DATA = {"timePeriod": "", "endTime": "", "instanceId": ""}
-
-URLS_DICT = {
-    "Apdex": URL_APDEX,
-    "Error": URL_ERRORS,
-    "TopN": URL_TOPN,
-    "Application": URL_APPLICATION,
-
-    "Throughput": URL_THROUGHPUT,
-    "ResponseQuantile": URL_OVERVIEW_QUANTILE,
-    "CPU": URL_CPU,
-    "Memory": URL_MEM,
-}
-# -------------------------------------------------------------------------------------------------
 
 
 class Transform(object):
